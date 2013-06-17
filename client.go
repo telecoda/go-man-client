6f235028e4ed49d418b4e21cf26ae401df7e5a69
telecoda <rob@thebaines.co.uk>
2013-06-17 06:50:35 +0100

    testing 1000 request per second

--- client.go
+++ client.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	//"github.com/telecoda/go-man/models"
+	"github.com/telecoda/go-man/models"
 	"net/http"
 	"time"
 )
@@ -13,34 +14,116 @@ func main() {
 
 	start := time.Now()
 	//response, err := http.Get("http://localhost:8080/games")
-	response, err := http.Post("http://localhost:8080/games", "application/json", nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
-	// clear screen
-	fmt.Print("\x0c")
-
-	fmt.Println(response.Status)
-	// get body
-	body := make([]byte, 1076)
-
-	count, err := response.Body.Read(body)
+	board, err := startNewGame()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	printBoard(board)
 
-	if count > 0 {
-		fmt.Println(string(body))
+	var gameId = board.Id
+
+	for i := 0; i < 1200; i++ {
+		fmt.Println(gameId)
+		fmt.Println(i)
+		board, err = getGame(gameId)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		printBoard(board)
+
 	}
-
-	response.Body.Close()
-
 	duration := time.Now().Sub(start)
 
 	fmt.Println(duration)
+
+}
+
+func startNewGame() (*models.GameBoard, error) {
+
+	response, err := http.Post("http://localhost:8080/games", "application/json", nil)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// get body
+	jsonBody := make([]byte, 2000)
+
+	count, err := response.Body.Read(jsonBody)
+
+	response.Body.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// reslice to actual bytes read
+	jsonBody = jsonBody[:count]
+
+	return convertJsonToBoard(jsonBody)
+}
+
+func getGame(gameId string) (*models.GameBoard, error) {
+
+	response, err := http.Get("http://localhost:8080/games/" + gameId)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// get body
+	jsonBody := make([]byte, 2000)
+
+	count, err := response.Body.Read(jsonBody)
+
+	response.Body.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// reslice to actual bytes read
+	jsonBody = jsonBody[:count]
+
+	return convertJsonToBoard(jsonBody)
+}
+
+func convertJsonToBoard(jsonBody []byte) (*models.GameBoard, error) {
+
+	var board models.GameBoard
+
+	err := json.Unmarshal(jsonBody, &board)
+
+	if err != nil {
+		fmt.Println("Error unmarshalling json", err)
+		return nil, err
+	}
+
+	return &board, nil
+
+}
+
+func printBoard(board *models.GameBoard) {
+
+	fmt.Println("Id:", board.Id)
+	fmt.Println("Name:", board.Name)
+	fmt.Println("Player:", board.MainPlayer.Location)
+
+	// upload board with players location
+	board.BoardCells[board.MainPlayer.Location.Y][board.MainPlayer.Location.X] = byte('M')
+
+	for _, row := range board.BoardCells {
+		//for _, cell := range row {
+		//fmt.Print(string(cell))
+		//}
+		fmt.Println(string(row))
+	}
 
 }
